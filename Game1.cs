6135015652ac826e5a0c56970564d45c94f64208
KRILLE123 <42100456+KRILLE123@users.noreply.github.com>
2020-10-08 13:24:53 +0200

    asd

--- Game1.cs
+++ Game1.cs
@@ -7,6 +7,8 @@ using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+
 
 namespace SkänkaSkeppV3
 {
@@ -16,6 +18,65 @@ namespace SkänkaSkeppV3
         public int[,] shipState { get; set; }
         public int tries { get; set; }
         public int numOfShips { get; set; }
+    }
+
+    public class Listener
+    {
+        Thread t = null;
+
+        public Listener()
+        {
+            t = new Thread(ThreadProc);
+            t.Start();
+        }
+
+        public void ThreadProc()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Game1.ServerRequest("listener", 1, 1);
+            }
+        }
+
+        public void ResetClient()
+        {
+            t = new Thread(ResetThread);
+            t.Start();
+        }
+        public void ResetThread()
+        {
+            Thread.Sleep(2500);
+            Game1.ResetGame();
+        }
+
+        public void EndThread()
+        {
+            t.Abort();
+        }
+    }
+
+    public class ResetThread
+    {
+        Thread t = null;
+
+        public ResetThread()
+        {
+
+            t = new Thread(ResetClient);
+            t.Start();
+
+            Thread.Sleep(2500);
+            Game1.ResetGame();
+        }
+
+        public void ResetClient()
+        {
+            Thread.Sleep(2500);
+            Game1.ResetGame();
+            t.Abort();
+        }
+
     }
 
     public class Game1 : Game
@@ -29,37 +90,78 @@ namespace SkänkaSkeppV3
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private SpriteFont font;
+
 
         private MouseState oldState;
         int xGrid = 8;
-        int yGrid = 5;
-        int maxTries = 10;
+        int yGrid = 4;
+        int maxTries = 5;
         static int state = 1;
+        Listener listener;
 
         static List<Player> Players = new List<Player>();
-        Color boxColor1 = new Color(150,0,0, 100);
-        Color boxColor2 = new Color(0, 100, 0, 100);
+        Color red = new Color(150, 0, 0, 100);
+        Color green = new Color(0, 100, 0, 100);
 
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
-            _graphics.IsFullScreen = false; 
+            _graphics.IsFullScreen = false;
 
             _graphics.ApplyChanges();
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-            Players.Add(new Player() { identifier = -1, numOfShips = 0, shipState = new int[8,5], tries = 0});
+            Players.Add(new Player() { identifier = -1, numOfShips = 0, shipState = new int[8, 5], tries = 0 });
+        }
+
+
+        public static void ResetGame()
+        {
+            Players.Clear();
+            state = 1;
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            StartClient("start",1,1);
+            ServerRequest("start", 1, 1);
+            listener = new Listener();
             base.Initialize();
+
         }
 
-        public static void StartClient(string action, int x, int y)
+        int GetCorrectSide(int i, int j)
+        {
+            if (Players[0].identifier == 1)
+            {
+                if (Players[0].numOfShips < 3 && Players[0].shipState[i, j] == 0 && i < (xGrid / 2))
+                {
+                    return 1;
+                }
+                else if (Players[0].numOfShips == 3 && i >= xGrid / 2)
+                {
+                    return 2;
+                }
+
+            }
+            else if (Players[0].identifier == 2)
+            {
+                if (Players[0].numOfShips < 3 && Players[0].shipState[i, j] == 0 && i >= (xGrid / 2))
+                {
+                    return 1;
+                }
+                else if (Players[0].numOfShips == 3 && i < xGrid / 2)
+                {
+                    return 2;
+                }
+            }
+            return 0;
+        }
+
+
+        public static void ServerRequest(string action, int x, int y)
         {
             byte[] bytes = new byte[1024];
 
@@ -86,7 +188,7 @@ namespace SkänkaSkeppV3
                     Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
 
                     byte[] msg = Encoding.ASCII.GetBytes(Players[0].identifier.ToString() + "/" + x.ToString() + "/" + y.ToString() + "/" + action);
-  
+
 
                     // Send the data through the socket.    
                     int bytesSent = sender.Send(msg);
@@ -95,24 +197,47 @@ namespace SkänkaSkeppV3
                     int bytesRec = sender.Receive(bytes);
                     string data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
 
-                    Debug.WriteLine(data);
-
                     if (action == "start")
                     {
                         Players[0].identifier = int.Parse(data);
-                    } 
-                    else if (data == "start:Search")
+                    }
+                    else if (data == "start")
                     {
-                        Players[0].numOfShips = 5;
+                        Players[0].numOfShips = 3;
                         state = 2;
-                    } 
-                    else if(data == "hit")
+                    }
+                    else if (data == "hit")
                     {
+                        Players[0].tries += 1;
                         Players[0].shipState[x, y] = 2;
-                    } 
-                    else if(data == "miss")
+                    }
+                    else if (data == "miss")
                     {
+                        Players[0].tries += 1;
                         Players[0].shipState[x, y] = 3;
+                    }
+                    else if (data.Contains("/"))
+                    {
+                        string[] dataSplit = data.Split("/");
+
+                        if (dataSplit[3] == "hit")
+                        {
+                            Players[0].shipState[int.Parse(dataSplit[1]), int.Parse(dataSplit[2])] = 2;
+                        }
+                        else if (dataSplit[3] == "miss")
+                        {
+                            Players[0].shipState[int.Parse(dataSplit[1]), int.Parse(dataSplit[2])] = 3;
+                        }
+                    } else if (data == "win")
+                    {
+                        state = 3;
+                        ResetClient();
+                    } else if (data == "lost")
+                    {
+                        state = 4;
+                    } else if (data == "draw")
+                    {
+                        state = 5;
                     }
                     sender.Shutdown(SocketShutdown.Both);
                     sender.Close();
@@ -138,7 +263,12 @@ namespace SkänkaSkeppV3
             }
         }
 
-    protected override void LoadContent()
+        protected override void UnloadContent()
+        {
+            listener.EndThread();
+        }
+
+        protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             battle_shipTexture = Content.Load<Texture2D>("battle_ship");
@@ -146,6 +276,7 @@ namespace SkänkaSkeppV3
             line = Content.Load<Texture2D>("line");
             wrong = Content.Load<Texture2D>("wrong");
             background = Content.Load<Texture2D>("background");
+            font = Content.Load<SpriteFont>("American Captain");
 
 
             // TODO: use this.Content to load your game content here
@@ -164,18 +295,24 @@ namespace SkänkaSkeppV3
                         bool insideGrid = newState.X >= i * 100 + 5 && newState.Y >= j * 100 && newState.X <= (i + 1) * 100 + 5 && newState.Y <= (j + 1) * 100;
                         if (insideGrid)
                         {
-                            if (Players[0].numOfShips < 5)
+                            int isRightSide = GetCorrectSide(i, j);
+
+                            Debug.WriteLine(state);
+                            if (state == 1)
                             {
-                                if (Players[0].shipState[i, j] == 0 && i < (xGrid/2))
+                                if (isRightSide == 1)
                                 {
-                                    Players[0].numOfShips++;
-                                    StartClient("add", i, j);
+                                    if (Players[0].shipState[i, j] == 0)
+                                    {
+                                        Players[0].numOfShips++;
+                                        Players[0].shipState[i, j] = 1;
+                                        ServerRequest("add", i, j);
+                                    }
                                 }
                             }
-                            else if (state == 2 && maxTries > Players[0].tries && i >= (xGrid/2))
+                            else if (state == 2 && maxTries > Players[0].tries && isRightSide == 2)
                             {
-                                StartClient("search", i, j);
-                                Players[0].tries += 1;
+                                ServerRequest("search", i, j);
                             }
                             else if (maxTries == Players[0].tries)
                             {
@@ -195,6 +332,7 @@ namespace SkänkaSkeppV3
                 Exit();
 
             Game_Control();
+            Debug.WriteLine(state);
 
 
             base.Update(gameTime);
@@ -211,38 +349,52 @@ namespace SkänkaSkeppV3
             {
                 for (int j = 0; j < yGrid; j++)
                 {
+                    int isRightSide = GetCorrectSide(i, j);
+                    Vector2 fixedPosWon = font.MeasureString("You won!");
+                    Vector2 fixedPosLost = font.MeasureString("You lost");
+                    Vector2 fixedPosDraw = font.MeasureString("It's a draw");
+
+
+                    _spriteBatch.DrawString(font, "Lives left: " + (maxTries - Players[0].tries), new Vector2(50, 410), Color.White);
+
+                    if(state == 3)
+                    {
+                        _spriteBatch.DrawString(font, "You won!",new Vector2(420 - (fixedPosWon.X/2), 410), Color.White);
+                    } else if (state == 4)
+                    {
+                        _spriteBatch.DrawString(font, "You lost!", new Vector2(420 - (fixedPosLost.X/2), 410), Color.White);
+                    } else if (state == 5)
+                    {
+                        _spriteBatch.DrawString(font, "It's a draw!", new Vector2(420 - (fixedPosDraw.X/2), 410), Color.White);
+                    }
+
+
                     if (Players[0].shipState[i, j] == 2)
                     {
                         _spriteBatch.Draw(battle_ship_destroyedTexture, new Vector2(i * 100 + 5, j * 100), null, Color.White, 0.0f, new Vector2(1.5f), new Vector2(0.2f), SpriteEffects.None, 1.0f);
-                    } else if (Players[0].shipState[i,j] == 3)
+                    }
+                    else if (Players[0].shipState[i, j] == 3)
                     {
                         _spriteBatch.Draw(wrong, new Vector2(i * 100 + 5, j * 100), null, Color.White, 0.0f, new Vector2(1.5f), new Vector2(0.2f), SpriteEffects.None, 1.0f);
-
+                    }
+                    else if (Players[0].shipState[i, j] == 1)
+                    {
+                        _spriteBatch.Draw(battle_shipTexture, new Vector2(i * 100 + 5, j * 100), null, Color.White, 0.0f, new Vector2(1.5f), new Vector2(0.2f), SpriteEffects.None, 1.0f);
                     }
                     else
                     {
-                        if (state == 2)
+                        if (isRightSide == 2 && Players[0].shipState[i, j] == 0)
                         {
-                            if (i < (xGrid / 2))
-                            {
-                                _spriteBatch.Draw(line, new Rectangle(i * 100 + 5, j * 100, 90, 90), boxColor2);
-                            }
-                            else if (i >= (xGrid / 2))
-                            {
-                                _spriteBatch.Draw(line, new Rectangle(i * 100 + 5, j * 100, 90, 90), boxColor1);
-
-                            }
-                        } else if (state == 1)
+                            _spriteBatch.Draw(line, new Rectangle(i * 100 + 5, j * 100, 90, 90), green);
+                        }
+                        else if (isRightSide == 1 && Players[0].shipState[i, j] == 0)
                         {
-                            if (i < (xGrid / 2))
-                            {
-                                _spriteBatch.Draw(line, new Rectangle(i * 100 + 5, j * 100, 90, 90), boxColor1);
-                            }
-                            else if (i >= (xGrid / 2))
-                            {
-                                _spriteBatch.Draw(line, new Rectangle(i * 100 + 5, j * 100, 90, 90), boxColor2);
+                            _spriteBatch.Draw(line, new Rectangle(i * 100 + 5, j * 100, 90, 90), green);
+                        }
+                        else if (isRightSide == 0 && Players[0].shipState[i, j] == 0)
 
-                            }
+                        {
+                            _spriteBatch.Draw(line, new Rectangle(i * 100 + 5, j * 100, 90, 90), red);
                         }
                     }
                 }
